31c92ed0e805624a9e417467b647328d89a2217e
khumbelo mulelu <khumbeloprince@gmail.com>
2024-05-09 08:37:18 +0200

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 export const routes: Routes = [
     {'path':'home',component:HomeComponent},
     {'path':'',redirectTo: '/home', pathMatch: 'full'},
-    // {'path':'signup',component:SignupComponent},
+    {'path':'about_us',component:AboutComponent},
+    {'path':'our_services',component:ServicesComponent},
+    {'path':'clients',component:ClientsComponent},
+    {'path':'gallery',component:GalleryComponent},
+    {'path':'contact',component:ContactComponent},
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TestimonialsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
